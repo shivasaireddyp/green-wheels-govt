2d122752c58889a451486a1ce279cd6c36634728
shivasaireddy <shivasaireddyp@gmail.com>
2024-03-01 13:24:21 +0530

    first commit

---
+++ src/components/RootLayout/RootLayout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import { ScrollRestoration } from "react-router-dom";
+
+function RootLayout() {
+  return (
+    <div>
+      <ScrollRestoration />
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+export default RootLayout;
